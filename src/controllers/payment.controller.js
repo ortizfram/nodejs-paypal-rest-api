985bbddf371167cdc5cf8ea1283080b26323d1fd
ortizfram <ortizfranco48@gmail.com>
2023-10-31 13:53:09 -0300

    controllers on routes

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,3 @@
+export const createOrder = (req, res) => res.send("Order created");
+export const captureOrder = (req, res) => res.send("Capture created");
+export const cancelPayment = (req, res) => res.send("Payment cancelled");
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import {
+  cancelPayment,
+  captureOrder,
+  createOrder,
+} from "../controllers/payment.controller.js";
 
 const router = Router();
 
-router.get("/create-order", (req, res) => res.send("Order created"));
-// ↓ when payment accepted
-router.get("/capture-order", (req, res) => res.send("Capture created"));
-router.get("/cancel-order", (req, res) => res.send("Payment cancelled"));
+router.get("/create-order", createOrder);
+router.get("/capture-order", captureOrder); // ← when payment accepted
+router.get("/cancel-order", cancelPayment);
 
 export default router;
