8b431ebf651203f19d2e410a11b190be2c151ca2
ortizfram <ortizfranco48@gmail.com>
2023-10-31 14:00:17 -0300

    config.js file for PORT HOST

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import paymentRoutes from './routes/payment.routes.js'
+import express from "express";
+import paymentRoutes from "./routes/payment.routes.js";
+import { HOST, PORT } from "./config.js";
 
 const app = express(); // declare express
 
-app.use(paymentRoutes)
+app.use(paymentRoutes);
 
-app.listen(3000);
-console.log('Server on port ', 3000);
+app.listen(PORT);
+console.log("Server on port ", PORT);
