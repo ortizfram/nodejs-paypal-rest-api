f196c020d0377c00793f291d8f44d5079846d8bf
ortizfram <ortizfranco48@gmail.com>
2023-11-02 11:27:44 -0300

    mongoose ogin schema collection

---
+++ src/mongodb.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+// connect mongodb to Nodejs
+mongoose
+  .connect("mongodb://localhost:27017/nodejs-paypal-rest-api")
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch(() => {
+    console.log("failed to connect db");
+  });
+
+// create schema
+const LoginSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+// Define collection
+const collection = new mongoose.model("Collection1", LoginSchema);
+
+// export collection
+module.exports = collection;
