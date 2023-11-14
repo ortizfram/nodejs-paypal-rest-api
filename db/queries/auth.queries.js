9cb90bba5a982304951e2fd92ed64ec6c942e190
ortizfram <ortizfranco48@gmail.com>
2023-11-14 09:17:39 -0300

    fiing dynamic queries

--- db/queries/auth.queries.js
+++ db/queries/auth.queries.js
@@ -1,3 +1,3 @@
     // db/queries/auth.queries.js
-    export const postSignupQuery = `INSERT INTO users (username, name, email, password) VALUES ($1, $2, $3, $4)`;
-    export const postLoginQuery= `SELECT * FROM users WHERE username = $1`;
+    export const postSignupQuery = `INSERT INTO users (username, name, email, password) VALUES (?, ?, ?, ?)`;
+    export const postLoginQuery= `SELECT * FROM users WHERE username = ?`;
