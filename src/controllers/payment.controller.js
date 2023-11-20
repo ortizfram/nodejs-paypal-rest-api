6eb7ad4392c887525b1b9d92ca9af79657ee8a38
ortizfram <ortizfranco48@gmail.com>
2023-11-20 13:47:01 -0300

    webhook working

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -6,19 +6,20 @@ import {
   PAYPAL_API_SECRET,
 } from "../config.js";
 import axios from "axios";
-import { getCourseFromSlugQuery, insertUserCourseQuery } from "../../db/queries/course.queries.js";
+import {
+  getCourseFromSlugQuery,
+  insertUserCourseQuery,
+} from "../../db/queries/course.queries.js";
 import { pool } from "../db.js";
 import { createTableUserCourses } from "../../db/queries/auth.queries.js";
-import mercadopago from 'mercadopago';
+import mercadopago from "mercadopago";
 import { config } from "dotenv";
 
-
-config();// load .ENV
-
+config(); // load .ENV
 
 // PAYPAL ---------------------------------------------------
 export const createOrderPaypal = async (req, res) => {
-  console.log("\n*** createOrderPaypal\n")
+  console.log("\n*** createOrderPaypal\n");
   try {
     const courseSlug = req.body.courseSlug; // is being passed the courseSlug in the request input
 
@@ -77,42 +78,49 @@ export const createOrderPaypal = async (req, res) => {
 
     // Log the created order
     console.log("\n--Created Order:", response.data);
-    
+
     // create user_courses table
     const [table] = await pool.query(createTableUserCourses);
     if (table.warningStatus === 0) {
-      console.log('\n---user_courses table created.\n');
+      console.log("\n---user_courses table created.\n");
     } else {
-      console.log('\n---user_courses table already exists.\n');
+      console.log("\n---user_courses table already exists.\n");
     }
 
     // paypal pay link
-    const approveLink = response.data.links[1].href; 
+    const approveLink = response.data.links[1].href;
 
     // Redirect the user to the PayPal approval link
     res.redirect(approveLink);
   } catch (error) {
     console.error("Error creating order:", error);
-    res.status(500).json({ message: "Error creating the order", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error creating the order", error: error.message });
   }
 };
 
 export const captureOrderPaypal = async (req, res) => {
-  console.log("\n*** captureOrderPaypal\n")
+  console.log("\n*** captureOrderPaypal\n");
   try {
     const { courseSlug } = req.query; //is obtained from the successful payment redirect query
     const user = req.session.user;
-    
-     // Fetch course details based on the courseSlug using MySQL query
-     const [rows] = await pool.query(getCourseFromSlugQuery, [courseSlug]);
-     const course = rows[0];
-     console.log("\n--Fetched Course:", course);
+
+    // Fetch course details based on the courseSlug using MySQL query
+    const [rows] = await pool.query(getCourseFromSlugQuery, [courseSlug]);
+    const course = rows[0];
+    console.log("\n--Fetched Course:", course);
 
     if (course && user) {
       // Add the user and course relationship in user_courses table
-      const [insertUserCourse] = await pool.query(insertUserCourseQuery, [user.id, course.id]);
+      const [insertUserCourse] = await pool.query(insertUserCourseQuery, [
+        user.id,
+        course.id,
+      ]);
       if (insertUserCourse.affectedRows > 0) {
-        console.log(`👌🏽 --Inserted into user_courses: User ID: ${user.id}, Course ID: ${course.id}`);
+        console.log(
+          `👌🏽 --Inserted into user_courses: User ID: ${user.id}, Course ID: ${course.id}`
+        );
       }
 
       return res.redirect(`/api/course/${courseSlug}/modules`);
@@ -134,7 +142,7 @@ export const createOrderMP = async (req, res) => {
   // • get course
   const courseSlug = req.body.courseSlug; // is being passed the courseSlug in the request input
   console.log(`\nSQL Query: ${getCourseFromSlugQuery}\n`);
-  console.log(`\nParameters: ${[courseSlug]}\n`);
+  console.log(`\ncourseSlug: ${[courseSlug]}\n`);
 
   // Fetch the course using the query
   const [rows] = await pool.query(getCourseFromSlugQuery, courseSlug);
@@ -142,7 +150,7 @@ export const createOrderMP = async (req, res) => {
   const course = rows[0];
 
   console.log(`\nFetched Course Details:`);
-  console.log(course)
+  console.log(course);
 
   // Check the type of course.price
   // console.log(`\nType of course.price: ${typeof course.price}\n`);
@@ -150,44 +158,80 @@ export const createOrderMP = async (req, res) => {
   // Convert course.price to a decimal
   const priceAsFloat = parseFloat(course.price);
 
-//♣
+  //♣
 
   mercadopago.configure({
     access_token: process.env.MP_SANDBOX_ACCESS_TOKEN,
   });
-  
+
   var preference = {
     items: [
       {
         title: course.title,
         quantity: 1,
-        currency_id: 'ARS',
+        currency_id: "ARS",
         unit_price: priceAsFloat,
-      }
+      },
     ],
     back_urls: {
-      success: "http://localhost:3000/api/success-mp",
+      success: `http://localhost:3000/api/course/${courseSlug}/modules`,
       failure: "http://localhost:3000/api/failure-mp",
       pending: "http://localhost:3000/api/pending-mp",
     },
     //here we use NGROK till it's deployed
-    notification_url: "https://b9a8-190-15-205-177.ngrok.io/api/webhook-mp",
+    notification_url: `https://d2d2-190-15-205-177.ngrok.io/api/webhook-mp?courseSlug=${courseSlug}`,
   };
-    
-  const result = await mercadopago.preferences.create(preference)
-  console.log(`\n--- MP preference created:`);
-  console.log(result)
+
+  const result = await mercadopago.preferences.create(preference);
+  console.log(`\n\n--- MP preference created:`);
+
+  // console.log(result.body);
+
+  // change on deployment
+  const initPoint = result.body.sandbox_init_point;
+  const redirectURL = `${initPoint}&courseSlug=${courseSlug}`;
+  res.redirect(redirectURL);
 };
 
+export const webhookMP = async (req, res) => {
+  console.log("\n\n*** Webhook MP...\n\n");
 
+  try {
+    const paymentType = req.query.type;
+    const paymentId = req.query["data.id"];
+    const courseSlug = req.query.courseSlug; // Ensure Mercado Pago sends courseSlug
+    console.log("courseSlug:", courseSlug);
+    console.log("paymentId:", paymentId);
+    console.log("paymentType:", paymentType);
 
-export const successMP = async(req, res) => {
-  res.send("\n*** Success MP...\n")
-}
+    if (paymentType === "payment" && paymentId && courseSlug) {
+      const user = req.session.user;
 
-export const webhookMP = async(req, res) => {
-  console.log("\n\n*** Webhook MP...\n\n")
+      // Fetch course details based on the courseSlug using MySQL query
+      const [rows] = await pool.query(getCourseFromSlugQuery, [courseSlug]);
+      const course = rows[0];
 
-  console.log(req.query)// ? type: 'payment' && topic: 'merchant_order'
-}
+      if (course && user) {
+        // Add the user and course relationship in user_courses table
+        const [insertUserCourse] = await pool.query(insertUserCourseQuery, [
+          user.id,
+          course.id,
+        ]);
 
+        if (insertUserCourse.affectedRows > 0) {
+          console.log(
+            `👌🏽 --Inserted into user_courses: User ID: ${user.id}, Course ID: ${course.id}`
+          );
+        }
+      }
+    }
+    res.sendStatus(204); // OK but none to return
+  } catch (error) {
+    console.error("Error handling Mercado Pago webhook:", error);
+    res.sendStatus(500);
+  }
+};
+
+export const successMP = async (req, res) => {
+  res.send("\n*** Success MP...\n");
+};
