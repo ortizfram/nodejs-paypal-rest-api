329f1afc1ffd70f618a045317378e2cfe331040c
melon <ortizfranco48@gmail.com>
2023-12-02 12:32:46 -0300

    create form edited

--- db/queries/course.queries.js
+++ db/queries/course.queries.js
@@ -5,7 +5,9 @@ export const createCourseTableQuery = `CREATE TABLE IF NOT EXISTS courses (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     slug VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
+    description VARCHAR(255 DEFAULT NULL,
+    text_content TEXT DEFAULT NULL,
+    video_link VARCHAR(255) NOT NULL,
     ars_price DECIMAL(10, 2) NOT NULL,
     usd_price DECIMAL(10, 2) NOT NULL,
     discount DECIMAL(10, 2) DEFAULT NULL,
@@ -17,75 +19,15 @@ export const createCourseTableQuery = `CREATE TABLE IF NOT EXISTS courses (
 );`;
 export const updateCourseQuery = `
 UPDATE courses
-SET title = ?, slug=?, description = ?, ars_price = ?, usd_price = ?, discount = ?, active = ?, thumbnail = ?, length = ?
+SET title = ?, slug=?, description = ?, text_content = ?, video_link =?, ars_price = ?, usd_price = ?, discount = ?, active = ?, thumbnail = ?, length = ?
 WHERE id = ?;
 `;
 export const createCourseQuery = `
-INSERT INTO courses (title, slug, description, ars_price, usd_price , discount, active, thumbnail, length)
+INSERT INTO courses (title, slug, description, text_content, video_link=?, ars_price, usd_price , discount, active, thumbnail, length)
 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
 `;
 export const deleteCourseQuery = `DELETE FROM courses WHERE id = ?;`;
 export const deleteUserCourseQuery = `DELETE FROM user_courses WHERE course_id = ?;`;
-
-// -----// -----------------
-
-// ----- Module Queries ------------------
-export const createTableModuleQuery = `
-CREATE TABLE IF NOT EXISTS modules (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(255) NOT NULL,
-  description TEXT DEFAULT NULL,
-  video_link VARCHAR(255) NOT NULL,
-  thumbnail VARCHAR(255) DEFAULT NULL,
-  courseId INT NOT NULL,
-  PRIMARY KEY(id),
-  FOREIGN KEY(courseId) REFERENCES courses(id) ON DELETE CASCADE
-);
-`;
-
-export const moduleCreateQuery = `
-INSERT INTO modules (courseId, title, description, video_link, thumbnail)
-VALUES (?, ?, ?, ?, ?);
-`;
-
-export const moduleUpdateQuery = `
-UPDATE modules
-SET title = ?, description = ?, video_link = ?, thumbnail = ?
-WHERE id = ?;
-`;
-
-export const modulesListQuery = `SELECT * FROM modules WHERE courseId = ?`;
-
-export const getExistingModulesQuery = `SELECT id, title, description, video_link, thumbnail
-FROM modules
-WHERE courseId = ?
-`;
-// -----// -----------------
-
-// // -----VIdeo Queries ------------------
-// export const createVideosTableQuery = `
-// CREATE TABLE IF NOT EXISTS videos (
-//     id INT NOT NULL AUTO_INCREMENT,
-//     courseId INT NOT NULL,
-//     module_id INT NOT NULL,
-//     video_id VARCHAR(100) NOT NULL,
-//     PRIMARY KEY(id),
-//     FOREIGN KEY(courseId) REFERENCES courses(id) ON DELETE CASCADE,
-//     FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE
-// )
-// `;
-
-// export const createVideoQuery = `
-// INSERT INTO videos (courseId, module_id, video_id)
-// VALUES (?, ?, ?, ?);
-// `;
-
-// export const listCourseVideosQuery = `SELECT * FROM videos WHERE courseId = ?`;
-
-// export const listModuleVideosQuery = `
-// SELECT * FROM videos WHERE courseId = ? AND moduleId = ?;
-// `;
-// -----// -----------------
 
 // -----Get course queries-----------------
 export const getCourseFromSlugQuery = `SELECT * FROM courses WHERE slug = ?`;
@@ -98,12 +40,11 @@ export const getUserEnrolledCoursesQuery = `
   FROM user_courses 
   WHERE user_id = ?;
 `;
-// -----// -----------------
 
 // -----Enroll course queries-----------------
 // stored as a comma-separated string
 export const insertUserCourseQuery = `INSERT INTO user_courses (user_id, course_id) VALUES (?, ?)`;
-// -----// -----------------
 
+// -----Checks queries-----------------
 export const tableCheckQuery = `SELECT 1 FROM information_schema.tables 
 WHERE table_name = ? LIMIT 1;`;
--- src/routes/courses.routes.js
+++ src/routes/courses.routes.js
@@ -11,14 +11,7 @@ router.get('/course/:id/update', controllers.getCourseUpdate);//get update
 router.post('/course/:id/update',  controllers.postCourseUpdate);// post update
 router.get('/course/:id/delete',  controllers.getCourseDelete);// get del
 router.post('/course/:id/delete',  controllers.postCourseDelete);// post del
-/////---------module-------------------------
-router.get("/course/:id/module/create", controllers.getModuleCreate);//get
-router.post('/course/:id/module/create', controllers.postModuleCreate);//post
-router.get('/course/:id/module/update', controllers.getModuleUpdate);//get update
-router.post('/course/:id/module/update', controllers.postModuleUpdate);//post update
-// /////---------video-------------------------
-// router.get("/course/:id/video/create", controllers.getVideoCreate);
-// router.post('/course/:id/video/create', controllers.postVideoCreate);
+
 //------------courseDetail  -------------------------
 router.get("/course/:id", controllers.courseOverview);
 router.get("/course/:id/enroll", controllers.courseEnroll);
