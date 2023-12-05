abb3c884fa67530cf528728287e4f83e3bf142c8
melon <ortizfranco48@gmail.com>
2023-12-05 16:48:54 -0300

    see roles

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,5 +5,6 @@ import  controller  from "../controllers/admin.controller.js";
 const router = Router();
 
 router.get("/users", controller.getUsers);
+// router.post("/users/change-role", controller....);
 
 export default router;
