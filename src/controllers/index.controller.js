7879e4fa12b1524d0173a460d86e4550e126a696
ortizfram <ortizfranco48@gmail.com>
2024-01-04 11:07:49 -0300

    navbar style

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -3,8 +3,12 @@ import { pool } from "../db.js";
 import sendEmailContactForm from "../utils/sendEmailContactForm.js";
 
 const getPing = async (req, res) => {
-  const [result] = await pool.query('SELECT "PONG" AS result');
-  res.json(result[0]);
+  // const [result] = await pool.query('SELECT "PONG" AS result');
+  // res.json(result[0]);
+  const message = req.query.message; // Retrieve success message from query params authcontroller
+  const user= req.session.user || null; 
+  
+  res.render('navbar2', {user,message});
 };
 
 const getHome = async (req, res) => {
