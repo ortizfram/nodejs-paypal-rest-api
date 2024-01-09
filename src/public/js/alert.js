11f0bbd10c7d2b7c5cb490e84c16c2511dc87791
melon <ortizfranco48@gmail.com>
2024-01-09 17:09:15 -0300

    change vercel .json 404

--- src/public/js/alert.js
+++
@@ -1,7 +0,0 @@
-// Automatically remove the alert message after a few seconds
-const alertElement = document.getElementById("alert");
-if (alertElement) {
-  setTimeout(() => {
-    alertElement.style.display = "none";
-  }, 5000); // 5000 milliseconds (5 seconds)
-}
