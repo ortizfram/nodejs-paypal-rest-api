4422d812398d36109496caf30659cf9633097a96
melon <ortizfranco48@gmail.com>
2024-01-11 04:41:57 -0300

    a

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // index.js
 import app from "./src/apps.js";
 import { config } from "dotenv";
-import {getHome} from "./src/controllers/index.controller.js"
 
 // load .ENV
 config();
@@ -14,6 +13,3 @@ const HOST = process.env.HOST || 'localhost';
 app.listen(PORT, HOST, () => {
   console.log(`Server is rrrunning on http://${HOST}:${PORT}`);
 });
-
-// Define Routes
-app.get('/', getHome);
