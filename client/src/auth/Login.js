70b88ee94c768c148bbf41f0771e31f99fe3cd84
ortizfram <ortizfranco48@gmail.com>
2024-01-23 12:31:28 -0300

    basic login form

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -5,90 +5,25 @@ import { useNavigate } from "react-router-dom";
 const URI = "http://localhost:5000/api/login";
 
 const CompLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-  const [backendMessage, setBackendMessage] = useState("");
 
-  const loginUser = async (email, password) => {
-    try {
-      const response = await axios.post(URI, {
-        email: email,
-        password: password,
-      });
-  
-      if (response.data.error) {
-        setBackendMessage(response.data.error);
-        console.error("Error logging in:", response.data.error);
-      } else if (response.data.user) {
-        setBackendMessage(response.data.message);
-        console.log("Login successful:", response.data.message);
-        navigate("/");
-      } else {
-        setBackendMessage("Unexpected response format. Please try again.");
-        console.error("Unexpected response format:", response.data);
-      }
-    } catch (error) {
-      setBackendMessage("An error occurred while logging in. Please try again.");
-      console.error("Error logging in:", error);
-    }
-  };
-  
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
-  const handleLoginSubmit = (e) => {
-    e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
-    loginUser(email, password);
-  };
-
+  const handleLogin = (e) =>{
+    e.preventDefault()
+    console.log({'username':username,'password':password})
+  }
 
   return (
-    <>
-      <div id="login-page-container">
-        <div id="login-page-content">
-
-        
-          <div className="section-title">
-            <h1 className="text-white">Ingresa</h1>
-          </div>
-
-          <form onSubmit={handleLoginSubmit} className="vertical-form">
-
-          {backendMessage && <p className="error-message">{backendMessage}</p>}
-
-        
-            <input
-              type="text"
-              value={email}
-              name="email"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              value={password}
-              name="password"
-              placeholder="contraseña"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input type="submit" value="Login" />
-          </form>
-          <div className="links-container">
-            <p>
-              <a className="text-white" href="/api/signup">
-                No tengo cuenta aún.
-              </a>
-            </p>
-            <p>
-              <a className="text-white" href="/api/forgot-password">
-                Olvidaste tu contraseña ?
-              </a>
-            </p>
-          </div>
-        </div>
-      </div>
-    </>
+     <div>
+      <form>
+        <label>Username</label>
+        <input type="text" onChange={(e)=>{setUsername(e.target.value)}}/>
+        <label>Password</label>
+        <input type="password"  onChange={(e)=>{setPassword(e.target.value)}}/>
+        <button onClick={handleLogin}>Login</button>
+      </form>
+     </div>
   );
 };
 
