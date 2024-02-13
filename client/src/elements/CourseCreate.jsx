87464dab8b93e9197966d98e21da738a89c4b5b6
melon <ortizfranco48@gmail.com>
2024-02-13 16:08:22 -0300

    form responsiveness

--- client/src/elements/CourseCreate.jsx
+++ client/src/elements/CourseCreate.jsx
@@ -82,121 +82,134 @@ const CourseCreate = () => {
 
   return (
     <>
-      <div id="create-course-container-page">
-        <div id="create-course-container" className="mt-8">
-          {user && <p className="text-primary">Hello, {user.id}!</p>}
+    <div id="create-course-container-page">
+      <div id="create-course-container" className="mt-8">
+        {user && <p className="text-primary">Hello, {user.id}!</p>}
 
-          <h1 className="section-title">Creando Curso</h1>
-          <div>
-            <form className="form" method="POST" onSubmit={handleSubmit}>
-              {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-              <h3>Titulo & contenido:</h3>
-              <label htmlFor="title">titulo:</label>
-              <br />
-              <input
-                name="title"
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-              />
-              <br />
-              <label htmlFor="description">Descripcion:</label>
-              <br />
-              <input
-                name="description"
-                onChange={handleChange}
-                type="text"
-                className="form-control"
+        <h1 className="section-title">Creando Curso</h1>
+        <div>
+          <form className="form" method="POST" onSubmit={handleSubmit}>
+            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+            <h3>Titulo & contenido:</h3>
+            <div className="row">
+              <div className="col-lg-6">
+                <label htmlFor="title">titulo:</label>
+                <input
+                  name="title"
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
                 />
-              <br />
-              <label htmlFor="text_content">contenido texto:</label>
-              <br />
-              <textarea
-                name="text_content"
-                onChange={handleChange}
-                type="text"
-                className="form-control"
-              ></textarea>
-              <br />
-              <h3>Subir Archivos:</h3>
-              {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-              <label htmlFor="video">subir video:</label>
-              <br />
-              <input
-                type="file"
-                name="video"
-                accept="video/*"
-                onChange={handleChange}
-              />
-              <br />
-              <hr />
-              <label htmlFor="image">subir miniatura:</label>
-              <br />
-              <input
-                type="file"
-                id="file"
-                name="image"
-                accept="image/*"
-                onChange={handleChange}
-              />
-              {/* preview */}
-              <div className="preview">
-                <img id="img" ref={$image} style={{ width: 300 }} />
               </div>
-              <br />
-              <hr />
-              <h3>Configurar precio</h3>
-              {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-              <label htmlFor="ars_price">ARS Price:</label>
-              <br />
-              <input
-                type="number"
-                id="ars_price"
-                name="ars_price"
-                onChange={handleChange}
-              />
-              <br />
-              <label htmlFor="usd_price">USD Price:</label>
-              <br />
-              <input
-                type="number"
-                id="usd_price"
-                name="usd_price"
-                onChange={handleChange}
-              />
-              <br />
-              <hr />
-              <h3>Adicionales & descuentos</h3>
-              {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
-              <label htmlFor="discount_ars">
-                descuento_ars opcional (numeros enteros):
-              </label>
-              <br /><strong>% ARS{" "}</strong>
-              <input
-                type="number"
-                id="discount_ars"
-                name="discount_ars"
-                onChange={handleChange}
-              />
-              <label htmlFor="discount_usd">
-                descuento_usd opcional (numeros enteros):
-              </label>
-              <br /><strong>% USD{" "}</strong>
-              <input
-                type="number"
-                id="discount_usd"
-                name="discount_usd"
-                onChange={handleChange}
-              />
-              <br />
-              <input type="hidden" name="author" value={user?.id || ''} />
-
-              <button type="submit">Create Course</button>
-            </form>
-          </div>
+              <div className="col-lg-6">
+                <label htmlFor="description">Descripcion:</label>
+                <input
+                  name="description"
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-12">
+                <label htmlFor="text_content">contenido texto:</label>
+                <textarea
+                  name="text_content"
+                  onChange={handleChange}
+                  type="text"
+                  className="form-control"
+                ></textarea>
+              </div>
+            </div>
+            <h3>Subir Archivos:</h3>
+            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+            <div className="row">
+              <div className="col-lg-6">
+                <label htmlFor="video">subir video:</label>
+                <input
+                  type="file"
+                  name="video"
+                  accept="video/*"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-6">
+                <label htmlFor="image">subir miniatura:</label>
+                <input
+                  type="file"
+                  id="file"
+                  name="image"
+                  accept="image/*"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-lg-12">
+                {/* preview */}
+                <div className="preview">
+                  <img id="img" ref={$image} style={{ width: 300 }} />
+                </div>
+              </div>
+            </div>
+            <h3>Configurar precio</h3>
+            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+            <div className="row">
+              <div className="col-lg-6">
+                <label htmlFor="ars_price">ARS Price:</label>
+                <input
+                  type="number"
+                  id="ars_price"
+                  name="ars_price"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-6">
+                <label htmlFor="usd_price">USD Price:</label>
+                <input
+                  type="number"
+                  id="usd_price"
+                  name="usd_price"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <h3>Adicionales & descuentos</h3>
+            {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+            <div className="row">
+              <div className="col-lg-6">
+                <label htmlFor="discount_ars">
+                  descuento_ars opcional (numeros enteros):
+                </label>
+                <strong>% ARS{" "}</strong>
+                <input
+                  type="number"
+                  id="discount_ars"
+                  name="discount_ars"
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="col-lg-6">
+                <label htmlFor="discount_usd">
+                  descuento_usd opcional (numeros enteros):
+                </label>
+                <strong>% USD{" "}</strong>
+                <input
+                  type="number"
+                  id="discount_usd"
+                  name="discount_usd"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <input type="hidden" name="author" value={user?.id || ''} />
+            <button type="submit">Create Course</button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
+  </>
   );
 };
 
