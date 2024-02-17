e44428ba0ab8d16c973225ea6e12d79df935dcad
melon <ortizfranco48@gmail.com>
2024-02-17 09:41:33 -0300

    video auto padding calc

--- client/src/elements/CourseDetail.jsx
+++ client/src/elements/CourseDetail.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
-import CompNavbar from '../template/Nabvar';
-import CompFooter from '../template/Footer';
+import CompNavbar from "../template/Nabvar";
+import CompFooter from "../template/Footer";
 import { UserContext } from "../hooks/UserContext.js";
-import '../public/css/course/courseDetail.css';
+import "../public/css/course/courseDetail.css";
 import AlertMessage from "../components/alertMessage.jsx";
 
 const CourseDetail = () => {
@@ -16,9 +16,9 @@ const CourseDetail = () => {
     const fetchCourse = async () => {
       try {
         const response = await fetch(`http://localhost:5002/api/course/${id}`, {
-          method: 'GET',
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         });
 
@@ -40,70 +40,75 @@ const CourseDetail = () => {
     return <div>Loading...</div>;
   }
 
-
   return (
     <>
-      <CompNavbar user={user}/>
+      <CompNavbar user={user} />
       <AlertMessage />
-      <div className="bg-course-detail">
-        <div className="background-blur"></div>
-        <div className="container" style={{ margin: "6.2rem auto" }}>
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <div className="video-container mb-1">
-                <video id="course-video" controls className="img-fluid shadow-lg">
-                  <source src={course.video} type="video/mp4" />
-                  <source src={course.video} type="video/webm" />
-                  <source src={course.video} type="video/ogg" />
-                  Your browser does not support the video tag.
-                </video>
+      <div className="containerr">
+        <div className="cont-course-detail" style={{ margin: "6.2rem auto" }}>
+          <div className="background-blur"></div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <div className="video-container mb-1">
+                  <video
+                    id="course-video"
+                    controls
+                    className="img-fluid shadow-lg"
+                  >
+                    <source src={course.video} type="video/mp4" />
+                    <source src={course.video} type="video/webm" />
+                    <source src={course.video} type="video/ogg" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="row">
-            <div className="col-lg-12 text-center">
-              <div>
-                <p>
-                  {course.updated_at}
-                  <span>
-                    <img
-                      className="avatar ms-4 me-2"
-                      src={course.author.avatar}
-                      alt="author_avatar"
-                    />
-                  </span>
-                  <span className="fw-bold">{course.author.username}</span> •
-                  <span>{course.author.name}</span>
-                  {user && user.role === "admin" && (
-                    <span className="course-admin-options opacity-50">
-                      <button className="btn">
-                        <p>
-                          <a
-                            className="text-muted"
-                            href={`/api/course/${course.id}/update?courseId=${course.id}`}
-                          >
-                            <i className="fas fa-edit me-2 text-white"></i>
-                          </a>
-                        </p>
-                      </button>
-                      <button className="btn">
-                        <p>
-                          <a
-                            className="text-muted"
-                            href={`/api/course/${course.id}/delete?courseId=${course.id}`}
-                          >
-                            <i className="fas fa-trash-alt me-2 text-white"></i>
-                          </a>
-                        </p>
-                      </button>
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <div>
+                  <p>
+                    {course.updated_at}
+                    <span>
+                      <img
+                        className="avatar ms-4 me-2"
+                        src={course.author.avatar}
+                        alt="author_avatar"
+                      />
                     </span>
-                  )}
-                </p>
-                <h2>{course.title}</h2>
-                <h6>{course.description}</h6>
+                    <span className="fw-bold">{course.author.username}</span> •
+                    <span>{course.author.name}</span>
+                    {user && user.role === "admin" && (
+                      <span className="course-admin-options opacity-50">
+                        <button className="btn">
+                          <p>
+                            <a
+                              className="text-muted"
+                              href={`/api/course/${course.id}/update?courseId=${course.id}`}
+                            >
+                              <i className="fas fa-edit me-2 text-white"></i>
+                            </a>
+                          </p>
+                        </button>
+                        <button className="btn">
+                          <p>
+                            <a
+                              className="text-muted"
+                              href={`/api/course/${course.id}/delete?courseId=${course.id}`}
+                            >
+                              <i className="fas fa-trash-alt me-2 text-white"></i>
+                            </a>
+                          </p>
+                        </button>
+                      </span>
+                    )}
+                  </p>
+                  <h2>{course.title}</h2>
+                  <h6>{course.description}</h6>
+                </div>
+                <p>{course.text_content}</p>
               </div>
-              <p>{course.text_content}</p>
             </div>
           </div>
         </div>
